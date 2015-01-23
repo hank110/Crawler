aa69c0932e5e4564c9d7c6b911d2fac57f787538
Hank Kim <hank@dm.snu.ac.kr>
2015-01-23 19:47:55 +0900

    all modules completed

--- test_run1_1.py
+++ test_run1_1.py
@@ -1,4 +1,5 @@
-#-*-coding:utf-8-*-
+#-*- coding: utf-8 -*-
+
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,34 +8,66 @@ from selenium.webdriver.support import expected_conditions as EC
 import json
 import re
 from selenium.common.exceptions import NoSuchElementException
+import datetime
+from datetime import timedelta
+import sys
+
+# to resolve encoding issue
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 browser = webdriver.Chrome()
-browser.get('http://s.weibo.com/weibo/%25E9%259F%25A9%25E5%259B%25BD?topnav=1&wvr=6&b=1')
+browser.get('http://s.weibo.com/weibo/%25E9%259F%25A9%25E5%259B%25BD&b=1&page=1')
+
 
 try:
     element = ui.WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "S_weibo")))
 finally:
-
     # denoting xpath for necessary componenets for contents of interest
     elements = browser.find_elements_by_xpath(".//p[@class='comment_txt']")
-    name = browser.find_elements_by_xpath(".//div[@class='feed_content wbcon']/a[1]")
+    name = browser.find_elements_by_xpath(".//a[@class='W_texta W_fb']")
     num_likes = browser.find_elements_by_xpath(".//span[@class='line S_line1']")
     time = browser.find_elements_by_xpath(".//div[@class='feed_from W_textb']/a[1]")
     test = browser.find_elements_by_xpath(".//div[@class='feed_from W_textb']")
     cellphone = browser.find_elements_by_xpath(".//a[@rel='nofollow']")
-           
-    # printing out time
-    # refering tweet = no, related article = no
-    for i in range(0, len(time)):
-        if not str(time[i].get_attribute("title")):
-            print "0"
+    
+    current_time = datetime.datetime.now()
+    
+    # url of each user acquired
+    for i in range(0,len(name)):
+        url = str(name[i].get_attribute("href"))
+        if 'http://weibo.com/p/' in url:
+            continue
         else:
-            print str(time[i].get_attribute("title"))
-            
+            print url
+    
+                    
     for i in range(0, len(test)):
         tt = test[i].text
-        print tt
-
+        splited = tt.split(" ")
+        time_part = splited[0].encode('utf8')
+        if "分" in time_part:
+            position = time_part.index('分')
+            minutedelta = int(time_part[0:position])
+            time_of_single_tweet = current_time - datetime.timedelta(minutes = minutedelta)
+            print time_of_single_tweet.strftime("%Y-%m-%d %H:%M:%S")
+        elif "秒" in time_part:
+            position = time_part.index('秒')
+            seconddelta = int(time_part[0:position])
+            time_of_single_tweet = current_time - datetime.timedelta(seconds = seconddelta)
+            print time_of_single_tweet.strftime("%Y-%m-%d %H:%M:%S")
+        elif "月" in time_part:
+            position1 = int(time_part.index('月'))
+            position2 = int(time_part.index('日'))
+            month = int(time_part[0:position1])
+            day = int(time_part[position1+3:position2])
+            print "%d-%d-%d %s" % (current_time.year, month, day, splited[1])
+        elif "今天" in time_part:
+            print "%d-%d-%d %s" % (current_time.year, current_time.month, current_time.day, splited[1])
+        else:
+            print time_part + " " + splited[1]
+            
+          
     # refering tweet = yes, related article = no
     for i in range(0, len(cellphone)):
         cell = cellphone[i].text
@@ -69,8 +102,11 @@ finally:
     # crawling IDs
     # refering tweet = yes, related article = no
     for i in range(0,len(name)):
-        print str(name[i].get_attribute("nick-name").encode('utf-8'))
-
+        if name[i].get_attribute("nick-name") is None:
+            continue
+        else:
+            print str(name[i].text.encode('utf-8'))
+            
     # crawling text (tweets in weibo)
     # refering tweet = yes, related article = no
     for i in range(0,len(elements)):
